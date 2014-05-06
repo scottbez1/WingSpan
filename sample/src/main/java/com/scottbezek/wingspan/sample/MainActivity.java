f069b9c7ac006a17971d940b64209d9f4b6fca82
Scott Bezek <scottbez1@gmail.com>
2014-05-06 00:15:28 -0700

    add another sample

--- sample/src/main/java/com/scottbezek/wingspan/sample/MainActivity.java
+++ sample/src/main/java/com/scottbezek/wingspan/sample/MainActivity.java
@@ -2,14 +2,19 @@ package com.scottbezek.wingspan.sample;
 
 import com.scottbezek.wingspan.WingSpan;
 import com.scottbezek.wingspan.WingSpan.ClickableSpanFactory;
+import com.scottbezek.wingspan.WingSpan.SpanFactory;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
+import android.text.style.RelativeSizeSpan;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import javax.annotation.Nonnull;
 
 
 public class MainActivity extends Activity {
@@ -18,8 +23,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout root = (LinearLayout)findViewById(R.id.demo_root);
 
-        Spanned foo = WingSpan.from("This is a <clickable>clickable test</clickable>")
+        TextView tv1 = new TextView(this);
+        tv1.setText(WingSpan.from("This is a <clickable>clickable test</clickable>")
                 .bind("clickable", new ClickableSpanFactory() {
                     @Override
                     protected void onClick(View widget) {
@@ -27,11 +34,23 @@ public class MainActivity extends Activity {
                                 .show();
                     }
                 })
-                .buildSpanned();
+                .buildSpanned());
+        tv1.setMovementMethod(new LinkMovementMethod());
+        root.addView(tv1, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
-        TextView tv = (TextView) findViewById(R.id.demo_text);
-        tv.setText(foo);
-        tv.setMovementMethod(new LinkMovementMethod());
+
+        TextView tv2 = new TextView(this);
+        tv2.setText(WingSpan.from(
+                "This is a <miniscule>miniscule element with <font color=\"#ff0000\">nested red text</font></miniscule>!!!")
+                .bind("miniscule", new SpanFactory() {
+                    @Nonnull
+                    @Override
+                    public Object getSpan(String tag, CharSequence innerContents) {
+                        return new RelativeSizeSpan(0.5f);
+                    }
+                })
+                .buildSpanned());
+        root.addView(tv2, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
     }
 
 }
